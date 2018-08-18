c01e152b1f85477ac4b5da43249e80a6b13fc09a
Ricardo Matias <ricardo.matias0@gmail.com>
2018-08-18 22:14:19 +0200

    many 2 many relationships

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
@@ -16,19 +19,77 @@ func main() {
 	db.DropTable(&User{})
 	db.CreateTable(&User{})
 
-	db.Model(&User{}).AddIndex("idx_first_name", "first_name")
-	db.Model(&User{}).AddUniqueIndex("idx_last_name", "last_name")
+	db.DropTable(&Calendar{})
+	db.CreateTable(&Calendar{})
+
+	db.DropTable(&Appointment{})
+	db.CreateTable(&Appointment{})
+
+	users := []User{
+		{Username: "robocop"},
+		{Username: "David Bowie"},
+		{Username: "super-derp"},
+	}
+
+	for i := range users {
+		db.Save(&users[i])
+	}
+
+	db.Debug().Save(&User{
+		Username: "batman",
+		Calendar: Calendar{
+			Name: "Improbable Events",
+			Appointments: []Appointment{
+				{
+					Subject:     "birthday",
+					Description: "foo",
+					Attendees:   users,
+				},
+				{
+					Subject:     "death",
+					Description: "foo",
+					Attendees:   users,
+				},
+			},
+		},
+	})
+
+	u := User{}
+	c := Calendar{}
+
+	db.First(&u).Related(&c, "calendar")
+
+	fmt.Println(c)
 }
 
 type User struct {
-	Model     gorm.Model
+	gorm.Model
 	Username  string
 	FirstName string
 	LastName  string
+	Calendar  Calendar
 }
 
-var users = []User{
-	User{Username: "iamsaitam", FirstName: "Ricardo", LastName: "Matias"},
-	User{Username: "batman", FirstName: "Bruce", LastName: "Wayne"},
-	User{Username: "superman", FirstName: "Clark", LastName: "Kent"},
+type Calendar struct {
+	gorm.Model
+	Name         string
+	UserID       uint
+	Appointments []Appointment `gorm:"polymorphic:owner"`
+}
+
+type Appointment struct {
+	gorm.Model
+	Subject     string
+	Description string
+	StartTime   time.Time
+	Length      uint
+	CalendarID  uint
+	OwnerID     uint
+	OwnerType   string
+	Attendees   []User `gorm:"many2many:appointment_user"`
+}
+
+type TaskList struct {
+	gorm.Model
+	Appointments []Appointment `gorm:"polymorphic:owner"`
 }
